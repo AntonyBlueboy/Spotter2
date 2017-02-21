9fa3300e3d1d62f8709fddb351955cf36c5c1e02
Anton Martinenko <eol.darkelf13@gmail.com>
2017-02-22 00:52:30 +0200

    4 commit - prepared theory Activity and layouts of field, drawer and toolbar - not tested

---
+++ app/src/main/java/ua/com/spottertest/spotter2/RecyclerAdapter.java
@@ -0,0 +1,47 @@
+package ua.com.spottertest.spotter2;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Rudolf on 21.02.2017.
+ */
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>{
+
+    ArrayList<String> arrayList = new ArrayList<>();
+
+    public RecyclerAdapter(ArrayList<String> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_theory_field, parent, false);
+        RecyclerViewHolder viewHolder = new RecyclerViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        holder.textView.setText(arrayList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.tx_item);
+        }
+    }
+}
---
+++ app/src/main/java/ua/com/spottertest/spotter2/TheoryActivity.java
@@ -0,0 +1,52 @@
+package ua.com.spottertest.spotter2;
+
+import android.content.ClipData;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+
+public class TheoryActivity extends AppCompatActivity {
+    ActionBarDrawerToggle drawerToggle;
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    DrawerLayout drawerLayout;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<String> arrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theory);
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        /*Подозрительное место, возможно здесь задается неразвертываемость ресайклера*/
+        recyclerView.setHasFixedSize(true);
+        String[] items = getResources().getStringArray(R.array.topics);
+        /*Цикл можно заменить на конвертацию*/
+        for (String item : items){
+            arrayList.add(item);
+        }
+        adapter = new RecyclerAdapter(arrayList);
+        recyclerView.setAdapter(adapter);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+     }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+}
